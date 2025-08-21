a8a1c950cc4fe8fa9eaeea73d9de432caec5395d
arnav <arnavsingla06@gmail.com>
2025-08-22 00:56:30 +0530

    j

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import joblib
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -29,4 +30,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
